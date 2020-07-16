bf33d7eeab1bebf40b941b691efd556fc9da3b1a
Sergey Petrelevich <petrelevich@yandex.ru>
2020-07-16 23:09:42 +0300

    L05 collections

---
+++ L04-qa/src/main/java/ru/otus/testing/example1/services/WeddingService.java
@@ -0,0 +1,7 @@
+package ru.otus.testing.example1.services;
+
+import ru.otus.testing.example1.model.Family;
+
+public interface WeddingService {
+    Family doWedding();
+}
--- L05-qa/src/main/java/ru/otus/testing/example1/services/WeddingService.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.testing.example1.services;
-
-import ru.otus.testing.example1.model.Family;
-
-public interface WeddingService {
-    Family doWedding();
-}
