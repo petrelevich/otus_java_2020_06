66980cd83e703bf2a189e52582682269c1bb5871
Sergey Petrelevich <petrelevich@yandex.ru>
2020-07-21 00:07:16 +0300

    L06 reflection

---
+++ L06-reflection/src/main/java/ru/otus/annotations/usage/EmailDemo.java
@@ -0,0 +1,36 @@
+package ru.otus.annotations.usage;
+
+
+import ru.otus.annotations.Email;
+import ru.otus.annotations.Immutable;
+import ru.otus.annotations.ImmutableEmail;
+
+public class EmailDemo {
+    public static void main(String[] args) {
+
+        Class<?> clazz = EmailUse.class;
+
+        System.out.println("immutableEmail:" + clazz.isAnnotationPresent(ImmutableEmail.class));
+        System.out.println("email:" + clazz.isAnnotationPresent(Email.class));
+        System.out.println("immutable:" + clazz.isAnnotationPresent(Immutable.class));
+
+        System.out.println("------------------");
+
+        Class<?> clazzChild = EmailUseChild.class;
+
+        System.out.println("immutableEmail:" + clazzChild.isAnnotationPresent(ImmutableEmail.class));
+        System.out.println("email:" + clazzChild.isAnnotationPresent(Email.class));
+        System.out.println("immutable:" + clazzChild.isAnnotationPresent(Immutable.class));
+
+    }
+
+    @ImmutableEmail
+    private static class EmailUse {
+
+    }
+
+    private static class EmailUseChild extends EmailUse {
+
+    }
+
+}
