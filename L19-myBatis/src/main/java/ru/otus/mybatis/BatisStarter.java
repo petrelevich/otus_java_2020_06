cb21b29a71cb288eff87cacb1f2049f21774762a
Sergey Petrelevich <petrelevich@yandex.ru>
2020-10-08 23:15:01 +0300

    L27 webSockets

--- L19-myBatis/src/main/java/ru/otus/mybatis/BatisStarter.java
+++ L19-myBatis/src/main/java/ru/otus/mybatis/BatisStarter.java
@@ -10,58 +10,49 @@ import java.sql.Savepoint;
 // Примеры сделаны в виде тестов.
 
 public class BatisStarter {
-  private static final String URL = "jdbc:h2:mem:testDB";
-  private final Connection connection;
+    private static final String URL = "jdbc:h2:mem:testDB";
+    private final Connection connection;
 
-  public BatisStarter() throws SQLException {
-    this.connection = DriverManager.getConnection(URL);
-    this.connection.setAutoCommit(false);
-  }
-
-  public void createTables() throws SQLException {
-    createTableTest();
-    createTablePerson();
-    createTableAddress();
-  }
-
-  private void createTableTest() throws SQLException {
-    try (PreparedStatement pst = connection.prepareStatement(
-        "create table test(id int, name varchar(50), address_id int)")) {
-      pst.executeUpdate();
+    public BatisStarter() throws SQLException {
+        this.connection = DriverManager.getConnection(URL);
+        this.connection.setAutoCommit(false);
     }
-  }
 
-  private void createTablePerson() throws SQLException {
-    try (PreparedStatement pst = connection.prepareStatement(
-        "create table person(id int, firstName varchar(50), lastName varchar(50))")) {
-      pst.executeUpdate();
+    public void createTables() throws SQLException {
+        execSqlUpdate("create table test(id int, name varchar(50), address_id int)");
+        execSqlUpdate("create table person(id int, firstName varchar(50), lastName varchar(50))");
+        execSqlUpdate("create table address(id int, personId int, city varchar(50))");
     }
-  }
 
-  private void createTableAddress() throws SQLException {
-    try (PreparedStatement pst = connection.prepareStatement(
-        "create table address(id int, personId int, city varchar(50))")) {
-      pst.executeUpdate();
+    public void dropTables() throws SQLException {
+        execSqlUpdate("drop table test");
+        execSqlUpdate("drop table person");
+        execSqlUpdate("drop table address");
     }
-  }
 
-  public void insertRecords() throws SQLException {
-    try (PreparedStatement pst = connection.prepareStatement(
-        "insert into test(id, name) values (?, ?)")) {
-      Savepoint savePoint = this.connection.setSavepoint("savePointName");
-      try {
-        int rowCount = 0;
-        for (int idx = 0; idx < 100; idx++) {
-          pst.setInt(1, idx);
-          pst.setString(2, "NameValue_" + idx);
-          rowCount += pst.executeUpdate();
+    public void insertRecords() throws SQLException {
+        try (var pst = connection.prepareStatement(
+                "insert into test(id, name) values (?, ?)")) {
+            Savepoint savePoint = this.connection.setSavepoint("savePointName");
+            try {
+                int rowCount = 0;
+                for (int idx = 0; idx < 100; idx++) {
+                    pst.setInt(1, idx);
+                    pst.setString(2, "NameValue_" + idx);
+                    rowCount += pst.executeUpdate();
+                }
+                this.connection.commit();
+                System.out.println("inserted rowCount:" + rowCount);
+            } catch (SQLException ex) {
+                this.connection.rollback(savePoint);
+                System.out.println(ex.getMessage());
+            }
         }
-        this.connection.commit();
-        System.out.println("inserted rowCount:" + rowCount);
-      } catch (SQLException ex) {
-        this.connection.rollback(savePoint);
-        System.out.println(ex.getMessage());
-      }
     }
-  }
+
+    private void execSqlUpdate(String sql) throws SQLException {
+        try (var pst = connection.prepareStatement(sql)) {
+            pst.executeUpdate();
+        }
+    }
 }
--- L19-myBatis/src/test/java/ru/otus/mybatis/BatisTests.java
+++ L19-myBatis/src/test/java/ru/otus/mybatis/BatisTests.java
@@ -8,10 +8,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,149 +24,155 @@ import ru.otus.mybatis.model.Address;
 import ru.otus.mybatis.model.Person;
 import ru.otus.mybatis.model.Sample;
 
-public class BatisTests {
-  private static SqlSessionFactory sqlSessionFactory;
+class BatisTests {
+    private static SqlSessionFactory sqlSessionFactory;
 
-  @BeforeAll
-  public static void beforeAll() throws IOException, SQLException {
-    String resource = "mybatis-config.xml";
-    InputStream inputStream = Resources.getResourceAsStream(resource);
+    @BeforeAll
+    public static void beforeAll() throws IOException, SQLException {
+        var resource = "mybatis-config.xml";
+        var inputStream = Resources.getResourceAsStream(resource);
 
-    sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
-    // Можно сконфигурировать и в коде:
-    // sqlSessionFactory.getConfiguration().addMapper(Sample.class);
-    // sqlSessionFactory.getConfiguration().addCache();
-    // sqlSessionFactory.getConfiguration()...
-  }
-
-  @BeforeEach
-  void beforEach() throws SQLException {
-      BatisStarter demo = new BatisStarter();
-      demo.createTables();
-      demo.insertRecords();
-  }
-
-  // Выбор одного объекта
-  @Test
-  void selectOne() {
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      Sample sample = session.selectOne("testMapper.selectTestOne", 1);
-      System.out.println("selected: " + sample);
-    }
-  }
-
-  // Выбор списка объектов, несколько параметров
-  @Test
-  void selectAll() {
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      Map<String, Integer> params = new HashMap<>();
-      params.put("minId", 50);
-      params.put("maxId", 55);
-      List<Sample> samples = session.selectList("testMapper.selectTestAll", params);
-      System.out.println("selected: " + samples);
-    }
-  }
-
-  // like, шаблоны
-  @Test
-  void selectLike() {
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      Map<String, String> params = new HashMap<>();
-      params.put("nameParam", "%2%");
-      List<Sample> samples = session.selectList("testMapper.selectTestLike", params);
-      System.out.println("selected like with nameParam: " + samples);
-
-      samples = session.selectList("testMapper.selectTestLike");
-      System.out.println("selected like without nameParam: " + samples);
-    }
-  }
-
-  // in -- select * from Test where id in (1,2,3,4)
-  @Test
-  void selectForEach() {
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      List<Integer> params = Arrays.asList(1, 2, 3, 4, 5);
-      List<Sample> samples = session.selectList("testMapper.selectTestForEach", params);
-      System.out.println("selectedForEach: " + samples);
-    }
-  }
-
-  @Test
-  void insert() {
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      Map<String, String> params = new HashMap<>();
-      params.put("id", "500");
-      params.put("name", "TestInsertovich");
-      int rowCount = session.insert("testMapper.insert", params);
-      System.out.println("inserted: " + rowCount);
-
-      Sample sample = session.selectOne("testMapper.selectTestOne", 500);
-      System.out.println("selected: " + sample);
-    }
-  }
-
-  // Маппинг через интерфейсы
-  @Test
-  void selectOneInterface() {
-    //Optional добавили в 3.5.0 (только для "интерфейсов")
-
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      SampleMapperInterface mapper = session.getMapper(SampleMapperInterface.class);
-      Optional<Sample> sample = mapper.findOne(1);
-      System.out.println("selected: " + sample);
-
-      Optional<Sample> testNotExists = mapper.findOne(-1);
-      System.out.println("selected: " + testNotExists);
-    }
-  }
-
-  @Test
-  void selectCached() {
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      Sample sample = session.selectOne("testMapper.selectTestOne", 1);
-      System.out.println("1 selected: " + sample);
-
-      session.selectOne("testMapper.selectTestOne", 1);
-      System.out.println("2 selected: " + sample);
-
-      session.selectOne("testMapper.selectTestOne", 1);
-      System.out.println("3 selected: " + sample);
-
-      session.selectOne("testMapper.selectTestOne", 4);
-      System.out.println("4 selected: " + sample);
+        // Можно сконфигурировать и в коде:
+        // sqlSessionFactory.getConfiguration().addMapper(Sample.class);
+        // sqlSessionFactory.getConfiguration().addCache();
+        // sqlSessionFactory.getConfiguration()...
     }
 
-    // Настройка кэша в TestMapper.xml и ehcache.xml
-  }
-
-  @Test
-  public void selectPersonByCity() {
-    try (SqlSession session = sqlSessionFactory.openSession()) {
-      PersonDao personDao = session.getMapper(PersonDao.class);
-      AddressDao addressDao = session.getMapper(AddressDao.class);
-
-      Person person = new Person();
-      person.setId(1);
-      person.setFirstName("John");
-      person.setLastName("Black");
-
-      Person person2 = new Person();
-      person2.setId(2);
-      person2.setFirstName("Ivan");
-      person2.setLastName("Ivanov");
-
-      Address address = new Address();
-      address.setId(1);
-      address.setPersonId(2);
-      address.setCity("Moscow");
-
-      personDao.insert(person);
-      personDao.insert(person2);
-      addressDao.insert(address);
-
-      List<Person> loadedPerson = personDao.selectByCity("Moscow");
-      System.out.println(loadedPerson);
+    @BeforeEach
+    void beforeEach() throws SQLException {
+        var demo = new BatisStarter();
+        demo.createTables();
+        demo.insertRecords();
     }
-  }
+
+    @AfterEach
+    void afterEach() throws SQLException {
+        var demo = new BatisStarter();
+        demo.dropTables();
+    }
+
+    // Выбор одного объекта
+    @Test
+    void selectOne() {
+        try (var session = sqlSessionFactory.openSession()) {
+            var sample = session.selectOne("testMapper.selectTestOne", 1);
+            System.out.println("selected: " + sample);
+        }
+    }
+
+    // Выбор списка объектов, несколько параметров
+    @Test
+    void selectAll() {
+        try (var session = sqlSessionFactory.openSession()) {
+            var params = new HashMap<String, Integer>();
+            params.put("minId", 50);
+            params.put("maxId", 55);
+            List<Sample> samples = session.selectList("testMapper.selectTestAll", params);
+            System.out.println("selected: " + samples);
+        }
+    }
+
+    // like, шаблоны
+    @Test
+    void selectLike() {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            Map<String, String> params = new HashMap<>();
+            params.put("nameParam", "%2%");
+            List<Sample> samples = session.selectList("testMapper.selectTestLike", params);
+            System.out.println("selected like with nameParam: " + samples);
+
+            samples = session.selectList("testMapper.selectTestLike");
+            System.out.println("selected like without nameParam: " + samples);
+        }
+    }
+
+    // in -- select * from Test where id in (1,2,3,4)
+    @Test
+    void selectForEach() {
+        try (var session = sqlSessionFactory.openSession()) {
+            var params = Arrays.asList(1, 2, 3, 4, 5);
+            var samples = session.selectList("testMapper.selectTestForEach", params);
+            System.out.println("selectedForEach: " + samples);
+        }
+    }
+
+    @Test
+    void insert() {
+        try (var session = sqlSessionFactory.openSession()) {
+            var params = new HashMap<String, String>();
+            params.put("id", "500");
+            params.put("name", "TestInsertovich");
+            int rowCount = session.insert("testMapper.insert", params);
+            System.out.println("inserted: " + rowCount);
+
+            Sample sample = session.selectOne("testMapper.selectTestOne", 500);
+            System.out.println("selected: " + sample);
+        }
+    }
+
+    // Маппинг через интерфейсы
+    @Test
+    void selectOneInterface() {
+        //Optional добавили в 3.5.0 (только для "интерфейсов")
+
+        try (var session = sqlSessionFactory.openSession()) {
+            var mapper = session.getMapper(SampleMapperInterface.class);
+            var sample = mapper.findOne(1);
+            System.out.println("selected: " + sample);
+
+            var testNotExists = mapper.findOne(-1);
+            System.out.println("selected: " + testNotExists);
+        }
+    }
+
+    @Test
+    void selectCached() {
+        try (var session = sqlSessionFactory.openSession()) {
+            var sample = session.selectOne("testMapper.selectTestOne", 1);
+            System.out.println("1 selected: " + sample);
+
+            session.selectOne("testMapper.selectTestOne", 1);
+            System.out.println("2 selected: " + sample);
+
+            session.selectOne("testMapper.selectTestOne", 1);
+            System.out.println("3 selected: " + sample);
+
+            session.selectOne("testMapper.selectTestOne", 4);
+            System.out.println("4 selected: " + sample);
+        }
+
+        // Настройка кэша в TestMapper.xml и ehcache.xml
+    }
+
+    @Test
+    void selectPersonByCity() {
+        try (var session = sqlSessionFactory.openSession()) {
+            var personDao = session.getMapper(PersonDao.class);
+            var addressDao = session.getMapper(AddressDao.class);
+
+            var person = new Person();
+            person.setId(1);
+            person.setFirstName("John");
+            person.setLastName("Black");
+
+            var person2 = new Person();
+            person2.setId(2);
+            person2.setFirstName("Ivan");
+            person2.setLastName("Ivanov");
+
+            var address = new Address();
+            address.setId(1);
+            address.setPersonId(2);
+            address.setCity("Moscow");
+
+            personDao.insert(person);
+            personDao.insert(person2);
+            addressDao.insert(address);
+
+            var loadedPerson = personDao.selectByCity("Moscow");
+            System.out.println(loadedPerson);
+        }
+    }
 }
